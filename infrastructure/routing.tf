53eed2e8d88ad415dd6611d17f7cd085bf6dd887
Julian Alfonso Ponce <ponce.julianalfonso@gmail.com>
2021-07-04 21:51:01 +1000

    Removed igw, nat gw, eip for now

--- infrastructure/routing.tf
+++ infrastructure/routing.tf
@@ -42,76 +42,76 @@ resource "aws_subnet" "public-subnet" {
 
 # Internet Gateway and NAT Gateway Configuration
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.vpc.id
+# resource "aws_internet_gateway" "igw" {
+#   vpc_id = aws_vpc.vpc.id
 
-  tags = {
-    Name = "${local.service_name_env}-internet-gateway"
-  }
-}
+#   tags = {
+#     Name = "${local.service_name_env}-internet-gateway"
+#   }
+# }
 
-resource "aws_eip" "eip" {
-  vpc        = true
-  depends_on = [aws_internet_gateway.igw]
+# resource "aws_eip" "eip" {
+#   vpc        = true
+#   depends_on = [aws_internet_gateway.igw]
 
-  tags = {
-    Name = "${local.service_name_env}-eip"
-  }
-}
+#   tags = {
+#     Name = "${local.service_name_env}-eip"
+#   }
+# }
 
-resource "aws_nat_gateway" "ngw" {
-  allocation_id = aws_eip.eip.id
-  subnet_id     = aws_subnet.public-subnet.id
+# resource "aws_nat_gateway" "ngw" {
+#   allocation_id = aws_eip.eip.id
+#   subnet_id     = aws_subnet.public-subnet.id
 
-  tags = {
-    Name = "${local.service_name_env}-nat-gateway"
-  }
-}
+#   tags = {
+#     Name = "${local.service_name_env}-nat-gateway"
+#   }
+# }
 
 # Public route table configuration
 
-resource "aws_route_table" "route-table-public" {
-  vpc_id = aws_vpc.vpc.id
+# resource "aws_route_table" "route-table-public" {
+#   vpc_id = aws_vpc.vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.igw.id
+#   }
 
-  tags = {
-    Name = "${local.service_name_env}-route-table-public"
-  }
-}
+#   tags = {
+#     Name = "${local.service_name_env}-route-table-public"
+#   }
+# }
 
-resource "aws_route_table_association" "route-table-public-subnet-association" {
-  subnet_id      = aws_subnet.public-subnet.id
-  route_table_id = aws_route_table.route-table-public.id
-}
+# resource "aws_route_table_association" "route-table-public-subnet-association" {
+#   subnet_id      = aws_subnet.public-subnet.id
+#   route_table_id = aws_route_table.route-table-public.id
+# }
 
 # Private route table configuration
 
-resource "aws_route_table" "route-table-private" {
-  vpc_id = aws_vpc.vpc.id
+# resource "aws_route_table" "route-table-private" {
+#   vpc_id = aws_vpc.vpc.id
 
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.ngw.id
-  }
+#   route {
+#     cidr_block     = "0.0.0.0/0"
+#     nat_gateway_id = aws_nat_gateway.ngw.id
+#   }
 
-  tags = {
-    Name = "${local.service_name_env}-route-table-private"
-  }
-}
+#   tags = {
+#     Name = "${local.service_name_env}-route-table-private"
+#   }
+# }
 
-resource "aws_route_table_association" "route-table-private-subnet-1-association" {
-  subnet_id      = aws_subnet.private-subnet-1.id
-  route_table_id = aws_route_table.route-table-private.id
-}
+# resource "aws_route_table_association" "route-table-private-subnet-1-association" {
+#   subnet_id      = aws_subnet.private-subnet-1.id
+#   route_table_id = aws_route_table.route-table-private.id
+# }
 
-resource "aws_route_table_association" "route-table-private-subnet-2-association" {
-  subnet_id      = aws_subnet.private-subnet-2.id
-  route_table_id = aws_route_table.route-table-private.id
-}
+# resource "aws_route_table_association" "route-table-private-subnet-2-association" {
+#   subnet_id      = aws_subnet.private-subnet-2.id
+#   route_table_id = aws_route_table.route-table-private.id
+# }
 
 # Add Private subnet's and default security group to SSM
 resource "aws_ssm_parameter" "private_subnets_ssm" {
